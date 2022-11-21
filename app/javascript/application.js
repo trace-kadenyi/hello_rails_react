9350a395418037b94d9739a34e31853f3e5cf605
kadenyi <treykadenyi@gmail.com>
2022-11-21 16:40:35 +0300

    Autogenerated files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const root = ReactDOM.render(<App />, document.getElementById("root"));
