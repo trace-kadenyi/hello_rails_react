1f500a87b4ab6c29e470c55c9a4154d777a79dce
kadenyi <treykadenyi@gmail.com>
2022-11-21 16:39:40 +0300

    Create five greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: "Hello there")
+Greeting.create(greeting: "HOla mi amigo")
+Greeting.create(greeting: "Bonjour, mon ami")
+Greeting.create(greeting: "Habari yako, rafiki")
+Greeting.create(greeting: "Ciao, cara")
